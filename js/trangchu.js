8c8d3a2041cef22e00f074c91314dd5130baa6fa
dat hoang <datdz016378122676@gmail.com>
2023-12-08 18:36:38 +0700

    6h36 8/12/2023

--- js/trangchu.js
+++ js/trangchu.js
@@ -17,9 +17,36 @@ leftBtn.addEventListener('click', function () {
     document.querySelector('.slider-content-left-top').style.right = index * 100 + "%"
     index = index - 1
     if (index < 0) {
-        index = max-1;
+        index = max - 1;
     }
 })
 
 //slider trượt khi click
+const listClick = document.querySelectorAll('.slider-content-left-bottom li')
 
+
+listClick.forEach(function (image, index) {
+    image.addEventListener('click', function () {
+        document.querySelector('.slider-content-left-top').style.right = index * 100 + "%"
+        removeActive()
+        image.classList.add('active')
+    })
+})
+function removeActive() {
+    let imagActive = document.querySelector('.active')
+    imagActive.classList.remove('active')
+}
+//slide tự trượt
+function imgAuto() {
+    index = index + 1;
+    
+    if (index > max - 1) {
+        index = 0;
+    }
+    removeActive()
+    document.querySelector('.slider-content-left-top').style.right = index * 100 + "%"
+    // console.log(index)
+    listClick[index].classList.add('active')
+}
+
+setInterval(imgAuto, 3000);
